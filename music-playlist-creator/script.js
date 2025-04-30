8f521cc223e9e8d626d0df751aaf795e31f64547
Erika Lee <erika.lee@codepath.org>
2025-04-30 14:45:19 -0700

    add rest of wireframes

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,1 +1,18 @@
+// JavaScript for Opening and Closing the Modal
+var modal = document.getElementById("playlist-modal");
+var span = document.getElementsByClassName("close")[0];
 
+function openModal(playlist) {
+   document.getElementById('playlist-title').innerText = playlist.title;
+   document.getElementById('playlist-creator').innerText = playlist.creator;
+   modal.style.display = "block";
+}
+
+span.onclick = function() {
+   modal.style.display = "none";
+}
+window.onclick = function(event) {
+   if (event.target == modal) {
+      modal.style.display = "none";
+   }
+}
